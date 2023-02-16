2af2233bc5604739316cba291d75783177ea79cf
GFrancV <gfrancv@hotmail.com>
2023-02-16 14:31:04 +0000

    feat(api-routes): Add the API routes and the methods.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import connectionDB from "./database/db";
 
+import apiRoutes from "./router/api.routes";
+
 connectionDB();
 
 const app = express();
+
+app.use("/api/", apiRoutes);
 
 export default app;
---
+++ src/router/api.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/tasks");
+router.get("/tasks/:id");
+router.post("/tasks");
+router.put("/tasks/:id");
+router.delete("/tasks/:id");
+
+export default router;
